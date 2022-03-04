43412c16fcac4e478deb9859caf9cbdd8c63e0a1
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-03-05 03:28:17 +0800

    0305 影片播放不可用，收拾完成

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -7,13 +7,9 @@ export DISPLAY=$DISPLAY
 export XMODIFIERS=$XMODIFIERS 
 export QT_IM_MODULE=$QT_IM_MODULE 
 export GTK_IM_MODULE=$GTK_IM_MODULE 
-export AUDIO_ID=`getent group audio | cut -d: -f3`
-export VIDEO_GID=`getent group video | cut -d: -f3`
-#export LC_ALL=$LC_ALL # 設了會妨礙輸入法
 export LANG=$LANG
 
-#docker-compose up
-#docker-compose run app gedit
-docker-compose run app bash
+docker-compose up
+#docker-compose run app bash
 
 
